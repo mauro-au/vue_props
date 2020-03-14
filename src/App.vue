f8f048aae6ce1b637e49a47008b3555247d61aab
mauriciodesign <mauricio.a.design@gmail.com>
2020-03-13 22:59:44 -0300

    vue props

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="main">
+    <h2>{{ message }}</h2>
+    <br />
+    <label>Tarea</label>
+    <input type="text" v-model="tarea" @keyup.enter="guardarTarea"/>
+    <button @click="guardarTarea">Agregar</button>
+    <br />
+    <AgregarLista :tareas="tareas" @antena= "borrarTarea"></AgregarLista>
+  </div>
+</template>
+
+<script>
+import AgregarLista from "./componentes/AgregarLista.vue";
+export default {
+  name: "App",
+  components: { 
+      AgregarLista 
+    },
+  data() {
+    return {
+      message: "Liata de Tareas",
+      tarea: "",
+      tareas: [],
+      mostrar: false
+    };
+  },
+  methods: {
+
+    guardarTarea: function() {
+        if(this.tarea !== ""){
+
+            this.tareas.push({
+              name: this.tarea
+            });
+        }
+      this.tarea = "";
+    },
+      
+		borrarTarea: function(index){
+			this.tareas.splice(index,1);
+		}
+  }
+};
+</script>
+<style scoped>
+#main {
+  font-size: 25px;
+  font-family: "Calibri", sans-serif;
+  color: darkblue;
+  text-align: center;
+}
+button{
+    padding: 10px 30px;
+    background: #0e62ff;
+    border-radius: 5px;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+}
+input{
+    padding: 10px;
+    margin: 0 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+
+</style>
---
+++ src/componentes/AgregarLista.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <ul class="container">
+            <li v-for="(tarea, index) in tareas" :key="index">
+                {{tarea.name}}
+                <button @click="enviarBorrado(index)">eliminar</button> 
+            </li>
+        </ul>
+
+    </div>
+</template>
+<script>
+    export default {
+    name: "AgregarLista",
+     props: {
+      tareas: Array
+      },
+
+      methods:{
+          enviarBorrado: function(index){
+              this.$emit('antena', index)
+          }
+      }
+};
+
+</script>
+
+<style scoped>
+
+button{
+    padding: 10px 30px;
+    background: #0e62ff;
+    border-radius: 5px;
+    border: none;
+    color: #fff;
+    margin-left: 15px;
+    cursor: pointer;
+}
+.container{
+    width: 370px;
+    margin: 30px auto 0 auto;
+    text-align: left;
+}
+.container li{
+    margin:20px;
+}
+</style>
